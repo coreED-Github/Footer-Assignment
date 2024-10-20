bd41ae3cae4bfa5d7291e74faff24b29691432eb
Saira Nasir <sairanasir853@gmail.com>
2024-10-20 05:55:13 -0700

    first commit

---
+++ src/app/components/app.tsx
@@ -0,0 +1,19 @@
+"use client"
+import React from "react";
+export default function App() {
+  return (
+<div>
+<footer className='flex flex-col space-y-10 justify-center m-32 mb-0'>
+<div className='flex justify-center space-x-5 transition-transorm duration-500 ease-in-out transform hover:scale-105'>
+           <img src='https://cdn-icons-png.flaticon.com/128/5968/5968764.png' alt='facebook logo' />
+          <img src='https://cdn-icons-png.flaticon.com/128/4494/4494497.png' alt='linkdin logo' />
+         <img src='https://cdn-icons-png.flaticon.com/128/4494/4494477.png' alt='instagram logo' />
+           <img src='https://cdn-icons-png.flaticon.com/128/1384/1384063.png' alt='instagram logo' />
+          </div>
+        <h3 className='text-center hover:text-cyan-600 text-blue-900 font-medium mb-8'>Created By Saira Nasir. </h3>
+              <br />
+        <br />
+      </footer>
+</div>
+  );
+}
